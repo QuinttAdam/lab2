6412a739004f98256f2d34687bcee0f1fa90e460
QuinttAdam <116001984+QuinttAdam@users.noreply.github.com>
2023-10-04 19:38:13 +0200

    feat: circles go animated to random coordinates

--- classes/World.js
+++ classes/World.js
@@ -1,4 +1,5 @@
-class World {
+import Island from "./Island.js";
+export default class World {
     constructor() {
       this.islands = []; // a good place to keep track of your islands
       this.hookEvents(); // let's kick things of by hooking up events
@@ -6,6 +7,12 @@ class World {
   
     hookEvents() {
       // hook events like clicking buttons to a specific function
+      // add event listener to the button with id="btnAddIsland"
+      document.querySelector("#btnAddIsland")
+        .addEventListener("click", (e) => {
+          e.preventDefault();
+          this.addIsland();
+        });
     }
   
     save() {
@@ -29,10 +36,31 @@ class World {
   
     addIsland(island) {
       // add the islands to the DOM
+      // let circle= new Island();
+      // circle.getRandomColor();
+      // circle.getRandomName()
+      this.getCoordinates();
+      this.moveIsland();
     }
   
     moveIsland(island) {
       // this might be a good point to animate the islands with JS Animations API
+      let div = document.createElement("div");
+        div.classList.add("island");
+        //append to body
+        document.body.appendChild(div);
+        
+        let coords = this.getCoordinates();
+        //use js web animations api to animate the island to coords
+        div.animate([
+            {transform: `translate(0px, 0px)`},
+            {transform: `translate(${coords.x}px, ${coords.y}px)`}
+        ], {
+            duration: 1000,
+            iterations : 1,
+            fill : "forwards",
+            
+        });
     }
   }
   
